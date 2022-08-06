40b995e643f7f7af2e517106207f86c06431f9de
Madan Gopal <mgopal.0001@gmail.com>
2022-08-07 03:49:56 +0530

    feat(datacenter): connection and user schema

---
+++ datacenter/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const User = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isVarified: {
+      type: Boolean,
+      required: true,
+    },
+    uuid: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: [config.user.roles.user, config.user.roles.admin],
+      default: config.user.roles.user,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Users", User, "Users");
